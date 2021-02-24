840232a2010b7ce7bf1b0a73e77a13d0a217322a
Bjorn Bruns <bbruns@deloitte.nl>
2021-02-24 12:15:17 +0100

    Initial commit

---
+++ config_template.py
@@ -0,0 +1,10 @@
+# config.py template
+# Add your respective folder names and save as config.py
+
+ROOT = ""
+PATH_CODE = ROOT + "Notebooks/"
+PATH_DATA = ROOT + "Data/"
+PATH_DATA_PROCESSED = ROOT + "Data/data_processed/"
+PATH_MODELS = ROOT + "trained_models/"
+# PATH_OUTPUT = ROOT + "Data/data_processed/"
+# PATH_METADATA = PATH_DATA + "..."
